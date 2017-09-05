69ac723c9b8f4c8c050f5aa6dfe3c8a354c42831
Bruno Léon <bruno.leon@unyonsys.com>
2017-09-05 15:58:26 +0200

    feat: allow to pass key as sting. Simplify dependancies writing

--- manifests/domain.pp
+++ manifests/domain.pp
@@ -4,11 +4,15 @@ define opendkim::domain (
     $pathkeys      = '/etc/opendkim/keys',
     $keytable      = 'KeyTable',
     $signing_table = 'SigningTable',
+    $key           = undef,
 ) {
     # $pathConf and $pathKeys must be without trailing '/'.
     # For example, '/etc/opendkim/keys'
 
     Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ] }
+    Package[$opendkim::package_name] ->
+    Opendkim::Domain[$name] ~>
+    Service[$opendkim::service_name]
 
     # Create directory for domain
     file { "${pathkeys}/${domain}":
@@ -16,31 +20,35 @@ define opendkim::domain (
         owner   => $opendkim::owner,
         group   => $opendkim::group,
         mode    => '0755',
-        notify  => Service[$opendkim::service_name],
-        require => Package[$opendkim::package_name],
     }
 
-    # Generate dkim-keys
-    exec { "opendkim-genkey -D ${pathkeys}/${domain}/ -d ${domain} -s ${selector}":
-        unless  => "/usr/bin/test -f ${pathkeys}/${domain}/${selector}.private && /usr/bin/test -f ${pathkeys}/${domain}/${selector}.txt",
-        user    => $opendkim::owner,
-        notify  => Service[$opendkim::service_name],
-        require => [ Package[$opendkim::package_name], File["${pathkeys}/${domain}"], ],
+    if $key {
+      file { "${pathkeys}/${domain}/${selector}.private":
+        ensure  => file,
+        content => $key,
+        mode    => '0640',
+        owner   => $opendkim::owner,
+        group   => $opendkim::group,
+      }
+    } else {
+      # Generate dkim-keys
+      exec { "opendkim-genkey -D ${pathkeys}/${domain}/ -d ${domain} -s ${selector}":
+          unless  => "/usr/bin/test -f ${pathkeys}/${domain}/${selector}.private && /usr/bin/test -f ${pathkeys}/${domain}/${selector}.txt",
+          user    => $opendkim::owner,
+          notify  => Service[$opendkim::service_name],
+          require => File["${pathkeys}/${domain}"],
+      }
     }
 
     # Add line into KeyTable
     file_line { "${opendkim::pathconf}/${keytable}_${domain}":
         path    => "${opendkim::pathconf}/${keytable}",
         line    => "${selector}._domainkey.${domain} ${domain}:${selector}:${pathkeys}/${domain}/${selector}.private",
-        notify  => Service[$opendkim::service_name],
-        require => Package[$opendkim::package_name],
     }
 
     # Add line into SigningTable
     file_line { "${opendkim::pathconf}/${signing_table}_${domain}":
         path    => "${opendkim::pathconf}/${signing_table}",
         line    => "*@${domain} ${selector}._domainkey.${domain}",
-        notify  => Service[$opendkim::service_name],
-        require => Package[$opendkim::package_name],
     }
 }
